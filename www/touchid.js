492685c06557e1223d5ca17862b0c3b0d9eec96d
avitalys <35559514+avitalys@users.noreply.github.com>
2018-08-09 13:43:01 +0300

    Update touchid.js
    Fix indentations

--- www/touchid.js
+++ www/touchid.js
@@ -1,30 +1,30 @@
 
 var argscheck = require('cordova/argscheck'),
-               exec = require('cordova/exec');
+    exec = require('cordova/exec');
 
 var touchid = {
-	isAvailable: function(successCallback, errorCallback){
-		exec(successCallback, errorCallback, "TouchID", "isAvailable", []);
-	},
-	save: function(key,password, successCallback, errorCallback) {
-		exec(successCallback, errorCallback, "TouchID", "save", [key,password]);
-	},
-	verify: function(key,message,successCallback, errorCallback){
-		exec(successCallback, errorCallback, "TouchID", "verify", [key,message]);
-	},
-	has: function(key,successCallback, errorCallback){
-		exec(successCallback, errorCallback, "TouchID", "has", [key]);
-	},
-	delete: function(key,successCallback, errorCallback){
-		exec(successCallback, errorCallback, "TouchID", "delete", [key]);
-	},
-	setLocale: function(locale,successCallback, errorCallback){
-		exec(successCallback, errorCallback, "TouchID", "setLocale", [locale]);
-	},
+    isAvailable: function(successCallback, errorCallback){
+        exec(successCallback, errorCallback, "TouchID", "isAvailable", []);
+    },
+    save: function(key,password, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "TouchID", "save", [key,password]);
+    },
+    verify: function(key,message,successCallback, errorCallback){
+        exec(successCallback, errorCallback, "TouchID", "verify", [key,message]);
+    },
+    has: function(key,successCallback, errorCallback){
+        exec(successCallback, errorCallback, "TouchID", "has", [key]);
+    },
+    delete: function(key,successCallback, errorCallback){
+        exec(successCallback, errorCallback, "TouchID", "delete", [key]);
+    },
+    setLocale: function(locale,successCallback, errorCallback){
+        exec(successCallback, errorCallback, "TouchID", "setLocale", [locale]);
+    },
     didFingerprintDatabaseChange: function(locale,successCallback, errorCallback){
         exec(successCallback, errorCallback, "TouchID", "didFingerprintDatabaseChange", [locale]);
-	},
-	verifyFingerprint: function(message,successCallback, errorCallback){
+    },
+    verifyFingerprint: function(message,successCallback, errorCallback){
         exec(successCallback, errorCallback, "TouchID", "verifyFingerprint", [message]);
     }
 };
